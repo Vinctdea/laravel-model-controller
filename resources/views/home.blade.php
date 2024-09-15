5659a131d84c8bbe254c20b38462599cc08811f8
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-15 16:56:43 +0200

    fix: route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,13 @@
 
 @section('content')
     <div class="container my-5">
-        <h1>{{ $title }}</h1>
+        <h1>Titolo</h1>
         <img src="{{ Vite::asset('resources/img/colibri.jpg') }}" alt="" class="img-fluid">
         <p>
-            {{ $text }}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, soluta! Enim molestias, reiciendis animi nihil
+            sed veritatis, ipsum pariatur quisquam molestiae hic illo, iure doloremque beatae? Cum reiciendis, nihil debitis
+            commodi consectetur, odio, nesciunt quasi exercitationem ratione quas modi eius incidunt id laudantium a
+            voluptates tempora voluptatibus est obcaecati similique!
         </p>
     </div>
 @endsection
