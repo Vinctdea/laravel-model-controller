f82ebe983b9612f54bd07a226762b5f7772b3ba2
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-15 16:36:23 +0200

    feat add page controller and model

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,13 +2,13 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container my-5">
-    <h1>Il nostro fantastico team</h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi, iusto dolore eaque laudantium optio, adipisci perspiciatis voluptate obcaecati sit perferendis natus velit ut! Tempora voluptatum ipsam modi rem voluptas.
-    </p>
-</div>
-
+    <div class="container my-5">
+        <h1>Il nostro fantastico team</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sunt sed harum enim aliquid omnis ab pariatur
+            vel ipsam libero, amet quia ipsa magni accusantium in accusamus temporibus id numquam?
+        </p>
+    </div>
 @endsection
 
 
