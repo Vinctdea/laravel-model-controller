89528a1b23560de63d51e1e7fdc012f8c8607c79
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-15 22:00:11 +0200

    fix: style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.main')
+
 @section('content')
-    <div class="container">
+    <div class="container d-flex  ">
 
         @foreach ($movies as $movie)
-            <div class="card" style="width: 18rem;">
+            <div class="card " style="width: 18rem;">
                 <img src="#" class="card-img-top" alt="{{ $movie->title }}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ $movie->description }}</p>
+                    <p class="card-text">{{ $movie->nationality }}</p>
+                    <p class="card-text">{{ $movie->original_title }}</p>
 
                 </div>
             </div>
